8de4b033e18d48228946343cb7ed3d8502c41109
Summer Saleh <summerkatherine1@gmail.com>
2017-07-19 12:24:06 -0400

    Delete CategoryRepository.java

--- spring-boot-mvc-jpa-reviewreview-master/src/main/java/org/wecancodeit/reviewreview/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.wecancodeit.reviewreview;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-
-}
