9491c6de4b2276640547816cc989230c057ce953
Summer Saleh <summerkatherine1@gmail.com>
2017-07-19 13:03:50 -0400

    deleted files

--- javascript-with-testing/spec/factorial-test.js
+++
@@ -1,8 +0,0 @@
-describe("should expect 1", function(func){
-	it("for 1", function() {
-		var actual = func(1);
-		expect(response).toBe(1);
-		return areEqual(expected, actual);
-	};
-
-	testFactorial_1(factorial);
--- javascript-with-testing/src/factorial.js
+++
@@ -1,8 +0,0 @@
-var areEqual = function(expected, actual) {
-	return expected === actual;
-};
-
-var factorial = function(n) {
-	return 1;
-};
-
