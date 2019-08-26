c131d8c17fbfdaca83c7cb16d99f56485b1d8a19
Summer Saleh <summersaleh@c001-mbpsselah.vcfcorp.com>
2019-08-26 14:52:12 -0400

    modal 1

--- js/navResponsive.js
+++ js/navResponsive.js
@@ -1,2 +1,11 @@
 // JS for nav
 
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav-container") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-container";
+    }
+}
+
